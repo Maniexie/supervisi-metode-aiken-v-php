7c4a9bc4145d2d64103ccfb720a6942c95203ffe
unknown <artiasifotri@gmail.com>
2025-12-19 22:16:19 +0700

    menambahkan daftar aktor

---
+++ pages/supervisi/template.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/../../pages/layouts/header.php';
+?>
+
+<!-- Cards/Content -->
+<section class="cards">
+    <div class="card">kelola item PENILAIAN 1</div>
+    <div class="card">kelola item PENILAIAN 2</div>
+    <div class="card">kelola item PENILAIAN 3</div>
+</section>
+</div>
+</div>
+
+<?php
+require_once __DIR__ . '/../../pages/layouts/footer.php';
+?>
